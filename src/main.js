90b866f34e9ff0ea9317a8314e14c0efc39fe312
windliang <648917857@qq.com>
2021-08-22 18:38:50 +0800

    feat: react

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
-document.write('hello world')
+// /src/main.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Hello extends React.Component {
+  render() {
+    return <div>Hello {this.props.toWhat}</div>;
+  }
+}
+
+ReactDOM.render(
+  <Hello toWhat="World by jsx" />,
+  document.getElementById('app')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,17 @@ module.exports = {
     path: path.resolve(__dirname, './dist'),
     filename: 'bundle.js'
   },
+  module: {
+    rules: [
+      {
+        test: /\.(js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
   devServer: {
     static: path.resolve(__dirname, './dist')
   }
