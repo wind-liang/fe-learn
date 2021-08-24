8503a67c1eb00e461e0e9876be1c752970e3ebfb
windliang <648917857@qq.com>
2021-08-24 09:21:10 +0800

    feat: sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 // /src/App.tsx
 import * as React from 'react';
 import { DatePicker } from 'antd';
+import './App.scss';
 
 type Props = {
   toWhat: string;
@@ -10,8 +11,9 @@ type State = {};
 class App extends React.Component<Props, State> {
   render(): JSX.Element {
     return (
-      <div>
-        Hello {this.props.toWhat}
+      <div className="app">
+        <div className="text">Hello</div>
+        <div>{this.props.toWhat}</div>
         <div>
           <DatePicker></DatePicker>
         </div>
